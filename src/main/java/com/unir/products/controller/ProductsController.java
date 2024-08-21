595b1f0c75352bcd472ed62b9fb86705ce5f8715
Kalio <kalio.ofvil@gmail.com>
2024-08-21 16:17:36 -0600

    Creating product project

---
+++ src/main/java/com/unir/products/controller/ProductsController.java
@@ -0,0 +1,87 @@
+package com.unir.products.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unir.products.model.pojo.Product;
+import com.unir.products.model.request.CreateProductRequest;
+import com.unir.products.service.ProductsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductsController
+{
+	
+	private final ProductsService service;
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> getProducts()
+	{
+		
+		List<Product> products = service.getProducts();
+		
+		if(products != null)
+		{
+			return ResponseEntity.ok(products);
+		} else
+		{
+			return ResponseEntity.ok(Collections.emptyList());
+		}
+	}
+	
+	@GetMapping("/products/{productId}")
+	public ResponseEntity<Product> getProduct(@PathVariable String productId) 
+	{
+		Product product = service.getProduct(productId);
+		
+		if (product != null)
+		{
+			return ResponseEntity.ok(product);
+		}
+		else
+		{
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/products/{productId}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable String productId)
+	{
+		Boolean removed = service.removeProduct(productId);
+		
+		if (Boolean.TRUE.equals(removed))
+		{
+			return ResponseEntity.ok().build();
+		}
+		else
+		{
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping("/products")
+	public ResponseEntity<Product> getProduct(@RequestBody CreateProductRequest request)
+	{
+		Product createdProduct = service.createProduct(request);
+		
+		if(createdProduct != null)
+		{
+			return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+		}
+		else
+		{
+			return ResponseEntity.badRequest().build();
+		}
+	}
+}
